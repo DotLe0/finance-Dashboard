d98f0cb1f2112e0f8f19f85b8a7e14421e145e5d
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2024-07-19 14:56:07 +0300

    basic UI compleated

---
+++ js/script.js
@@ -0,0 +1,4 @@
+let incomeJanuary = document.getElementById("incomeJanuary");
+let expensesJanuary = document.getElementById("expensesJanuary");
+let savedJanuary = incomeJanuary - expensesJanuary;
+console.log(savedJanuary)
