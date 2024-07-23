c1875c34f6c64591121c11c16544c888d55a05e1
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2024-07-23 22:01:24 +0300

    improvementsToJS

--- js/script.js
+++ js/script.js
@@ -1,12 +1,29 @@
-let incomeJanuary = document.getElementById("incomeJanuary");
-let expensesJanuary = document.getElementById("expensesJanuary");
-let savedJanuary = document.getElementById("savedJanuary")
 let btn = document.getElementById("btn");
 
+function calculateAll() {
+    const inputs = document.querySelectorAll("input");
+    const income = Array.from(inputs).slice(0,12).map(incom => {return incom.value;});
+    const expenses = Array.from(inputs).slice(12,24).map(expens => {return expens.value;});
+    console.log(income)
+    console.log(expenses)
+}
+
 function calculateJanuary() {
+    let incomeJanuary = document.getElementById("incomeJanuary");
+    let expensesJanuary = document.getElementById("expensesJanuary");
+    let savedJanuary = document.getElementById("savedJanuary")
     let one = incomeJanuary.value;
     let two = expensesJanuary.value;
     let result = one - two;
 
     savedJanuary.innerHTML = result;
+}
+
+function calculateFeburary() {
+    let incomeFeburary = document.getElementById("incomeFebruary");
+    let expensesFeburary = document.getElementById("expensesFebruary");
+    let savedFebruary = document.getElementById("savedFebruary");
+
+    let result = incomeFeburary.value - expensesFeburary.value;
+    savedFebruary.innerHTML = result;
 }
