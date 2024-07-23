cab321c90d7cd1a1870fa3f6f660bffee8bff0b8
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2024-07-23 17:20:18 +0300

    update

--- js/script.js
+++ js/script.js
@@ -1,4 +1,12 @@
 let incomeJanuary = document.getElementById("incomeJanuary");
 let expensesJanuary = document.getElementById("expensesJanuary");
-let savedJanuary = incomeJanuary - expensesJanuary;
-console.log(savedJanuary)
+let savedJanuary = document.getElementById("savedJanuary")
+let btn = document.getElementById("btn");
+
+function calculateJanuary() {
+    let one = incomeJanuary.value;
+    let two = expensesJanuary.value;
+    let result = one - two;
+
+    savedJanuary.innerHTML = result;
+}
