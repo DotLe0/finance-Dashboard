58ba7750b0e3ee1d828e6cccf3c4e1ced481aa87
DotLe0 <78364935+DotLe0@users.noreply.github.com>
2024-07-24 09:18:00 +0300

    some what working chart

--- js/script.js
+++ js/script.js
@@ -1,14 +1,35 @@
 let btn = document.getElementById("btn");
 
+//Getting all input values
+const inputs = document.querySelectorAll("input");
+
+//spliting the income and expenses into two arrays
+const income = Array.from(inputs).slice(0,12).map(incom => {return incom.value;});
+const expenses = Array.from(inputs).slice(12,24).map(expens => {return expens.value;});
+
+console.log(income)
+
+const ctx = document.getElementById('incomeChart');
+
+  new Chart(ctx, {
+    type: 'bar',
+    data: {
+      labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'],
+      datasets: [{
+        label: 'Income Chart',
+        data: income,
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
 function calculateAll() {
-    //Getting all input values
-    const inputs = document.querySelectorAll("input");
-    
-    //spliting the income and expenses into two arrays
-    const income = Array.from(inputs).slice(0,12).map(incom => {return incom.value;});
-    const expenses = Array.from(inputs).slice(12,24).map(expens => {return expens.value;});
-
-
     //calculating the saved money by subtracting expenses[i] from income[i]
     const resultArray = [];
 
